b278e0ec7fb1d89d0cee45a385490768c314411b
Nils Rokita <0rokita@informatik.uni-hamburg.de>
2019-03-26 07:42:00 +0100

    Add initial suport for fedora, fixes to readme

--- install_deps.sh
+++ install_deps.sh
@@ -1,12 +1,22 @@
 #! /bin/bash
 
-mkdir ../wordir
-cd ../workdir
 
-
-sudo apt install git python-virtualenv osm2pgsl python-pip python-dev gcc postgis
+if hash apt-get 2>/dev/null; then
+    echo "Using apt-get to install"
+    sudo apt-get install git python-virtualenv osm2pgsl python-pip python-dev gcc postgis
+elif hash dfn 2>/dev/null; then
+    echo "Using DFN to install"
+    sudo dnf install osm2pgsql python2-virtualenv git python2-pip python2-devel postgis gcc
+else
+    echo "Unknown package manager"
+    exit 1
+fi
 
 sudo adduser tileserver --home /opt/tileserver/ --system
+
+
+mkdir ~tileserver/wordir
+cd ~tileserver/workdir
 
 # todo: add postgress account
 
@@ -17,6 +27,7 @@ sudo adduser tileserver --home /opt/tileserver/ --system
 echo "declare -A PORTS\nPORTS[dummy]=8000">port_mapping
 touch server_list
 
+chown -hR tileserver ~tileserver
 
 # initial nginx frontend confi + certs???
 #### maybe via subdomain from map-data.de
